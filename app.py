03a622c3751b7e85bd4cfcce2b14972516477e23
Abishek Vasudevan <harishabishek7@gmail.com>
2021-02-07 06:52:56 -0500

    still working, song is not being read properly

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import os
 from flask import Flask, render_template
+import p1m1
 
 app = Flask(__name__)
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 
 
 @app.route('/')
-# def main():
-app.run(host=os.getenv("IP", "0.0.0.0"), port=int(
-    os.getenv("PORT", 8080)), debug=True)
+def main():
+    song = p1m1.p1m1_main()
+    return render_template(
+        "index.html",
+        **song
+    )
+
+
+app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True)
