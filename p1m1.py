7248ad8d7906f4cdf373e99850959a6639605219
Abishek Vasudevan <harishabishek7@gmail.com>
2021-02-07 06:53:40 -0500

    made functions so that app.py can access

--- p1m1.py
+++ p1m1.py
@@ -61,26 +61,28 @@ def get_top_song_of_artist(artist_id):
     return data["tracks"]
 
 
-artists_id = ["1mYsTxnqsietFxj1OgoGbG", "5mqguTgtaoCMNMZD6txCh6", "0gxyHStUsqpMadRV0Di1Qt"]
+def p1m1_main():
+    artists_id = ["1mYsTxnqsietFxj1OgoGbG", "5mqguTgtaoCMNMZD6txCh6", "0gxyHStUsqpMadRV0Di1Qt"]
 
-artist_num = random.randint(0, len(artists_id) - 1)
-top_tracks = get_top_song_of_artist(artists_id[artist_num])
+    artist_num = random.randint(0, len(artists_id) - 1)
+    top_tracks = get_top_song_of_artist(artists_id[artist_num])
 
-song_num = random.randint(0, len(top_tracks) - 1)
-song = top_tracks[song_num]
+    song_num = random.randint(0, len(top_tracks) - 1)
+    song = top_tracks[song_num]
 
-# print(js.dumps(song, indent=4, sort_keys=True))
-# print(song["preview_url"])
-song = {
-    "name": song["name"],
-    "song_url": song["external_urls"]["spotify"],
-    "artists": [artist["name"] for artist in song["artists"]],
-    "artist_links": [artist["external_urls"]["spotify"] for artist in song["artists"]],
-    "image_url": song["album"]["images"][0]["url"],
-    "album_name": song["album"]["name"],
-    "album_release_date": song["album"]["release_date"],
-    "length": song["duration_ms"],
-    "song_preview": song["preview_url"] if (song["preview_url"] != "null") else None
-}
+    # print(js.dumps(song, indent=4, sort_keys=True))
+    # print(song["preview_url"])
+    song = {
+        "name": song["name"],
+        "song_url": song["external_urls"]["spotify"],
+        "artists": [artist["name"] for artist in song["artists"]],
+        "artist_links": [artist["external_urls"]["spotify"] for artist in song["artists"]],
+        "image_url": song["album"]["images"][0]["url"],
+        "album_name": song["album"]["name"],
+        "album_release_date": song["album"]["release_date"],
+        "length": song["duration_ms"],
+        "song_preview": song["preview_url"] if (song["preview_url"] != "null") else None
+    }
+    return song
 # print("------------------------------------------")
 # print(js.dumps(song, indent=4))
