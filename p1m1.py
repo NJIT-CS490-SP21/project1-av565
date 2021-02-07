3062e8e33ad31a3158b7f427995256ac7f91be75
Abishek Vasudevan <harishabishek7@gmail.com>
2021-02-07 05:42:50 -0500

    Somewhat done with main py

--- p1m1.py
+++ p1m1.py
@@ -2,6 +2,7 @@ import os
 import requests
 import time
 import dotenv
+import random
 import json as js
 
 dotenv_file = dotenv.find_dotenv()
@@ -25,12 +26,16 @@ def get_new_access_token():
 
 
 def create_access_token(t=time.time()):
+    global dotenv_file
+    token = os.getenv("TOKEN")
     if t - float(os.getenv("TOKEN_TIMEOUT")) >= 3600:
-        dotenv.set_key(dotenv_file, "TOKEN", get_new_access_token())
+        token = get_new_access_token()
+        dotenv.set_key(dotenv_file, "TOKEN", token)
         dotenv.set_key(dotenv_file, "TOKEN_TIMEOUT", str(time.time()))
         print("Creating a new token")
+    dotenv_file = dotenv.find_dotenv()
     dotenv.load_dotenv(dotenv_file)
-    return str(os.getenv("TOKEN"))
+    return token
 
 
 access_token = create_access_token()
@@ -56,29 +61,26 @@ def get_top_song_of_artist(artist_id):
     return data["tracks"]
 
 
-# url = base_url + "browse/new-releases"
-
-# number_of_songs = 10
-# params = {"country": "US", "limit": number_of_songs, "offset": 0}
-
-# response = requests.get(url=url, headers=headers, params=params)
-
-# data = response.json()
-# for i in range(number_of_songs):
-#     print(data["albums"]["items"][i]["name"])
-
-# url = base_url + "playlists/37i9dQZEVXbMDoHDwVN2tF"
-# params = {'q': "Global Top 50", "type": "playlist"}
-# response = requests.get(url=url, headers=headers)
-
-# data = response.json()
-# for track in data["tracks"]["items"]:
-#     artists = [artist["name"] for artist in track["track"]["album"]["artists"]]
-#     out_string = "{} BY: {}".format(track["track"]["album"]["name"], ", ".join(artists))
-#     print(out_string)
-# print(data["tracks"]["items"][4]["track"]["album"])
-
-# print(js.dumps(data, indent=4, sort_keys=True))
-
 artists_id = ["1mYsTxnqsietFxj1OgoGbG", "5mqguTgtaoCMNMZD6txCh6", "0gxyHStUsqpMadRV0Di1Qt"]
-print(get_top_song_of_artist(artists_id[1])[0]["name"])
+
+artist_num = random.randint(0, len(artists_id) - 1)
+top_tracks = get_top_song_of_artist(artists_id[artist_num])
+
+song_num = random.randint(0, len(top_tracks) - 1)
+song = top_tracks[song_num]
+
+# print(js.dumps(song, indent=4, sort_keys=True))
+# print(song["preview_url"])
+song = {
+    "name": song["name"],
+    "song_url": song["external_urls"]["spotify"],
+    "artists": [artist["name"] for artist in song["artists"]],
+    "artist_links": [artist["external_urls"]["spotify"] for artist in song["artists"]],
+    "image_url": song["album"]["images"][0]["url"],
+    "album_name": song["album"]["name"],
+    "album_release_date": song["album"]["release_date"],
+    "length": song["duration_ms"],
+    "song_preview": song["preview_url"] if (song["preview_url"] != "null") else None
+}
+# print("------------------------------------------")
+# print(js.dumps(song, indent=4))
