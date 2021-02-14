1f58f2cd862a31d5f3cab24f242c298d3e1b0226
Abishek Vasudevan <harishabishek7@gmail.com>
2021-02-13 23:16:08 -0500

    Name Changes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template
-import p1m1
+import project1
 
 app = Flask(__name__)
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
@@ -8,7 +8,7 @@ app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 
 @app.route('/')
 def main():
-    song = p1m1.p1m1_main()
+    song = project1.project1_main()
     return render_template(
         "index.html",
         song=song
